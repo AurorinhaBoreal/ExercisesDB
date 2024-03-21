dddf14857ad2e8be916c1e18ac77058ab436e487
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-03-21 10:20:20 -0300

    Deleting Unecessary Files

--- app/src/main/java/exercises/logic/Age2.java
+++
@@ -1,7 +0,0 @@
-package exercises.logic;
-
-public class Age2 {
-    public static void TestAge() {
-        System.out.println("Testing Age Sucessful!");
-    }
-}
--- app/src/main/java/exercises/logic/Test.java
+++
@@ -1,8 +0,0 @@
-package exercises.logic;
-
-public class Test {
-    
-    public static void TestingImports() {
-        System.out.println("The test was sucessful!!");
-    }
-}
