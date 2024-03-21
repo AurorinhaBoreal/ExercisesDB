d46adb3afeb9f7fe3e173f4a8e5944cdcf1450e7
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-03-21 12:50:22 -0300

    :sparkles: feat: Select Product Exercise

--- app/src/main/java/exercises/App.java
+++ app/src/main/java/exercises/App.java
@@ -2,7 +2,8 @@ package exercises;
 
 
 import java.util.Scanner;
-import exercises.logic.Age;
+import exercises.logic.AgeExercise;
+import exercises.logic.ProductExercise;
 
 public class App {
     
@@ -11,17 +12,22 @@ public class App {
         Scanner sExercise = new Scanner(System.in);
 
         System.out.println("Escolha o exercicio que deseja executar:");
-        System.out.printf("1 - Age");
+        System.out.printf("1 - Age \t 2 - Product");
 
         exercise = sExercise.nextLine();
         switch (exercise) {
             case "1":
-                System.out.println("Você escolheu o exercicio 1...");
-                Age idade = new Age();
+                System.out.println("Você escolheu o exercicio Age...");
+                AgeExercise idade = new AgeExercise();
                 idade.VerifyingAge();
                 break;
-        
+            case "2":
+                System.out.println("Você escolheu o exercicio Product...");
+                ProductExercise produto = new ProductExercise();
+                produto.choice();
+                break;
             default:
+                System.out.println("Esse não é um exercicio válido.");
                 break;
         }
         sExercise.close();
