44c8c9eba41f934601e8f6eac825c7c035140caa
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-03-22 15:39:04 -0300

    :sparkles: feat: Select Calculator Exercise

--- app/src/main/java/exercises/App.java
+++ app/src/main/java/exercises/App.java
@@ -7,29 +7,33 @@ import exercises.logic.*;
 public class App {
     
     public static void main(String[] args) {
-        String exercise;
+        int exercise;
         Scanner sExercise = new Scanner(System.in);
 
         System.out.println("Escolha o exercicio que deseja executar:");
         System.out.printf("1 - Age \t 2 - Product \t 3 - Jokenpo");
 
-        exercise = sExercise.nextLine();
+        exercise = sExercise.nextInt();
         switch (exercise) {
-            case "1":
+            case 1:
                 System.out.println("Você escolheu o exercicio Age...");
                 AgeExercise idade = new AgeExercise();
                 idade.VerifyingAge();
                 break;
-            case "2":
+            case 2:
                 System.out.println("Você escolheu o exercicio Product...");
                 ProductExercise produto = new ProductExercise();
                 produto.productArray();
                 break;
-            case "3":
+            case 3:
                 System.out.println("Você escolheu o exercicio Jokenpo...");
-                Jokenpo jokenpo = new Jokenpo();
+                JokenpoExercise jokenpo = new JokenpoExercise();
                 jokenpo.game();
                 break;
+            case 4:
+                System.out.println("Você escolheu o exercicio Calculadora...");
+                CalculatorExercise calculator = new CalculatorExercise();
+                calculator.chooseCalc();
             default:
                 System.out.println("Esse não é um exercicio válido.");
                 break;
