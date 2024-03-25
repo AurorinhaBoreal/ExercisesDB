335ecd1407f77f5b450044ddd97beddaad768b6a
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-03-25 09:58:28 -0300

    :sparkles: Exercise 8 | Read

--- app/src/main/java/exercises/App.java
+++ app/src/main/java/exercises/App.java
@@ -13,7 +13,7 @@ public class App {
 
         System.out.println("Escolha o exercicio que deseja executar:");
         System.out.printf("1 - Idade \t 2 - Produto \t 3 - Jokenpo \t 4 - Calculadora \t 5 - Tabuada\n");
-    System.out.println("6 - Numeros Aleatórios \t 7 - Fibonacci");
+        System.out.println("6 - Numeros Aleatórios \t 7 - Fibonacci \t 8 - Leitura");
 
         exercise = sExercise.nextInt();
         switch (exercise) {
@@ -51,6 +51,11 @@ public class App {
                 System.out.println("Você escolheu o exercicio Fibonacci,,,");
                 FibonacciExercise fibonacci = new FibonacciExercise();
                 fibonacci.FibonacciCalculation();
+                break;
+            case 8:
+                System.out.println("Você escolheu o exercicio Leitura...");
+                ReadExercise read = new ReadExercise();
+                read.showNumber();
                 break;
             default:
                 System.out.println("Esse não é um exercicio válido.");
---
+++ app/src/main/java/exercises/logic/ReadExercise.java
@@ -0,0 +1,17 @@
+package exercises.logic;
+
+import java.util.Scanner;
+
+public class ReadExercise {
+    Scanner scanner = new Scanner(System.in);
+    int number;
+    int i;
+    public void showNumber() {
+        System.out.println("Digite um numero ai!");
+        while (number != 10) {
+            number = scanner.nextInt();
+        System.out.println("Um passarinho me contou que você digitou o número "+number+"!");
+        }
+        System.out.println("Você descobriu que numero secreto era 10! Parabens!");
+    }
+}
