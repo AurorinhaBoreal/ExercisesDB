955c9f61f6a8139ea1a936105312bbcc556287a4
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-03-27 10:38:57 -0300

    :recycle: refactor: Refactor Select Exercises Part 2

--- app/src/main/java/exercises/App.java
+++ app/src/main/java/exercises/App.java
@@ -12,103 +12,105 @@ import exercises.oop.*;
 import exercises.repetitionStructures.*;
 
 public class App {
-    
+
     public static void main(String[] args) {
         int chooseExercise;
+        String defaultMsg = "Você escolheu o exercicio";
         List<String> exercises = Arrays.asList("Idade", "Produto", "Jokenpo", "Calculadora", "Tabuada", "Numeros Aleatórios", "Fibonacci", "Leitura", "Limite de Fibonacci", "Soma de Matriz", "Organização de Array", "Mesclagem de Array", "Média de Aluno", "Conversão de Segundos", "Calculadora Estática", "Empréstimo de Livros", "Árvore Genealógica", "Automóveis", "Agenda Telefônica", "Super Mercado");
         Scanner sExercise = new Scanner(System.in);
 
+        showExercise(exercises);
+
+        chooseExercise = sExercise.nextInt();
+        
+        showChoosenExercise(defaultMsg, exercises, chooseExercise);
+        switch (chooseExercise) {
+            case 1:
+                AgeExercise idade = new AgeExercise();
+                idade.VerifyingAge();
+                break;
+            case 2:
+                ProductExercise produto = new ProductExercise();
+                produto.productArray();
+                break;
+            case 3:
+                JokenpoExercise jokenpo = new JokenpoExercise();
+                jokenpo.game();
+                break;
+            case 4:
+                CalculatorExercise calculator = new CalculatorExercise();
+                calculator.chooseCalc();
+                break;
+            case 5:
+                MultiplicationExercise multiplication = new MultiplicationExercise();
+                multiplication.choosingNumber();
+                break;
+            case 6:
+                RandomExercise random = new RandomExercise();
+                random.randomNumbers();
+                break;
+            case 7:
+                FibonacciExercise fibonacci = new FibonacciExercise();
+                fibonacci.FibonacciCalculation();
+                break;
+            case 8:
+                ReadExercise read = new ReadExercise();
+                read.showNumber();
+                break;
+            case 9:
+                FibonacciLimitExercise fibonacciLimit = new FibonacciLimitExercise();
+                fibonacciLimit.FibonacciCalculation();
+                break;
+            case 10:
+                MatrixSumExercise matrix = new MatrixSumExercise();
+                matrix.Matrix();
+                break;
+            case 11:
+                RandomArrayExercise array = new RandomArrayExercise();
+                array.Array();
+                break;
+            case 12:
+                TwoArraysExercise twoArrays = new TwoArraysExercise();
+                twoArrays.mergeArrays();
+                break;
+            case 13:
+                StudentExercise student = new StudentExercise();
+                student.studentInfo();
+                break;
+            case 14:
+                SecondsConversionExercise seconds = new SecondsConversionExercise();
+                seconds.getSeconds();
+                break;
+            case 15:
+                StaticCalculatorExercise.chooseCalc();
+                break;
+            case 16:
+                BookLoanExercise bookLoan = new BookLoanExercise();
+                bookLoan.BookListPopulate();
+                break;
+            case 17:
+                FamilyTree tree = new FamilyTree();
+                tree.populatingTree();
+            default:
+                System.out.println("Esse não é um exercicio válido.");
+                break;
+        }
+        sExercise.close();
+    }
+
+    public static void showExercise(List<String> exercises) {
+        System.out.println("|================EXERCICIOS===============|");
         for (int i = 0; i < exercises.size(); i++) {
             System.out.printf("| "+((exercises.indexOf(exercises.get(i)))+1)+" - "+exercises.get(i)+" |");
             if (i > 1) {
                 if (i%2 == 0) System.out.println("\n");    
             }
         }
-        chooseExercise = sExercise.nextInt();
-        switch (chooseExercise) {
-            case 1:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                AgeExercise idade = new AgeExercise();
-                idade.VerifyingAge();
-                break;
-            case 2:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                ProductExercise produto = new ProductExercise();
-                produto.productArray();
-                break;
-            case 3:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                JokenpoExercise jokenpo = new JokenpoExercise();
-                jokenpo.game();
-                break;
-            case 4:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                CalculatorExercise calculator = new CalculatorExercise();
-                calculator.chooseCalc();
-                break;
-            case 5:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                MultiplicationExercise multiplication = new MultiplicationExercise();
-                multiplication.choosingNumber();
-                break;
-            case 6:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                RandomExercise random = new RandomExercise();
-                random.randomNumbers();
-                break;
-            case 7:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                FibonacciExercise fibonacci = new FibonacciExercise();
-                fibonacci.FibonacciCalculation();
-                break;
-            case 8:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                ReadExercise read = new ReadExercise();
-                read.showNumber();
-                break;
-            case 9:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                FibonacciLimitExercise fibonacciLimit = new FibonacciLimitExercise();
-                fibonacciLimit.FibonacciCalculation();
-                break;
-            case 10:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                MatrixSumExercise matrix = new MatrixSumExercise();
-                matrix.Matrix();
-                break;
-            case 11:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                RandomArrayExercise array = new RandomArrayExercise();
-                array.Array();
-                break;
-            case 12:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                TwoArraysExercise twoArrays = new TwoArraysExercise();
-                twoArrays.mergeArrays();
-                break;
-            case 13:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                StudentExercise student = new StudentExercise();
-                student.studentInfo();
-                break;
-            case 14:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                SecondsConversionExercise seconds = new SecondsConversionExercise();
-                seconds.getSeconds();
-                break;
-            case 15:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                StaticCalculatorExercise.chooseCalc();
-                break;
-            case 16:
-                System.out.println("Você escolheu o exercicio "+exercises.get(chooseExercise-1)+"...");
-                BookLoanExercise bookLoan = new BookLoanExercise();
-                bookLoan.BookListPopulate();
-                break;
-            default:
-                System.out.println("Esse não é um exercicio válido.");
-                break;
-        }
-        sExercise.close();
+        System.out.println("\n|====================|");
+        System.out.println("Informe o Exercicio que deseja visualizar:");
+    }    
+
+    public static void showChoosenExercise(String defaultMsg, List<String> exercises, int chooseExercise) {
+        System.out.println(defaultMsg+" "+exercises.get(chooseExercise-1)+"...");
     }
 }
