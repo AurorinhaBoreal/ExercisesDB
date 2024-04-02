6ecf6c7d7fd35d3e4e35f34a8dc0df067912d60e
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-04-02 13:44:37 -0300

    :construction: Construction of Product

---
+++ app/src/main/java/exercises/superMarket/Product.java
@@ -0,0 +1,56 @@
+package exercises.superMarket;
+
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int stockQtt;
+
+    public Product(String name, double price, int stockQtt) {
+        this.name = name;
+        this.price = price;
+        this.stockQtt = stockQtt;
+    }
+
+    public String getProduct() {
+        return " | "+this.id+" | "+this.name+" | "+this.price+" | "+this.stockQtt+" |"; 
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    
+    public void stockUpdate(int qtt) {
+        this.stockQtt += qtt;
+    }
+
+    public void setStock(int stockQtt) {
+        this.stockQtt = stockQtt;
+    }
+    
+    public int getStock() {
+        return this.stockQtt;
+    }
+    
+}
