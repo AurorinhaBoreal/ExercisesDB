4f824599a1f4fe8d5aa3286edb41eccdb33a7d20
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-04-02 13:45:24 -0300

    :construction: Construction of Stock

---
+++ app/src/main/java/exercises/superMarket/Stock.java
@@ -0,0 +1,107 @@
+package exercises.superMarket;
+
+import java.util.ArrayList;
+public class Stock {
+    private int id = 1;
+    private ArrayList<Product> productsList = new ArrayList<>();
+
+    public void startStock() {
+
+    }
+
+    public Product findProduct(String name) {
+        int[] productFounded = {-1};
+        productsList.forEach((product) -> {
+            if ((productsList.get(productsList.indexOf(product)).getName()).equals(name)) {
+                productFounded[0] = productsList.indexOf(product);
+            }
+        });
+        return productsList.get(productFounded[0]);
+    }
+
+    public Product findProduct(int id) {
+        int[] productFounded = {-1};
+        productsList.forEach((product) -> {
+            if ((productsList.get(productsList.indexOf(product)).getId()) == id) {
+                productFounded[0] = productsList.indexOf(product);
+            }
+        });
+        return productsList.get(productFounded[0]);
+    }
+
+    // public boolean registerProduct(Product product) {
+    //     boolean registerVerify;
+    //     if (registerVerify) registerVerify = true;
+    //     else registerVerify = false;
+    //     return registerVerify;
+    // }
+
+    public void showStockCatalog() {
+        System.out.println("Stock ID:"+this.id);
+        productsList.forEach((product) -> {
+            System.out.println(product.getName());
+        });
+    }
+
+    public boolean updateStock(String name, int qtt) {
+        boolean updateVerify;
+        int productIndex[] = {0};
+        productsList.forEach((product) -> {
+            if ((productsList.get(productsList.indexOf(product)).getName()).equals(name)) {
+                productIndex[0] = productsList.indexOf(product);
+            }
+        });
+
+        try {
+            productsList.get(productIndex[0]).stockUpdate(qtt);
+            updateVerify = true;
+        } catch (Exception e) {
+            updateVerify = false;
+        }
+        return updateVerify;
+    }
+
+    public boolean updateStock(int id, int qtt) {
+        boolean updateVerify;
+        int productIndex[] = {0};
+        productsList.forEach((product) -> {
+            if ((productsList.get(productsList.indexOf(product)).getId()) == id) {
+                productIndex[0] = productsList.indexOf(product);
+            }
+        });
+
+        try {
+            productsList.get(productIndex[0]).stockUpdate(qtt);
+            updateVerify = true;
+        } catch (Exception e) {
+            updateVerify = false;
+        }
+        return updateVerify;
+    }
+
+    public int stockLatestQtt(Product product) {
+        int productQtt = 0;
+
+        return productQtt;
+    }
+
+    public int productListPosition(Product product) {
+        int prodIndex;
+        prodIndex = productsList.indexOf(product);
+        return prodIndex;
+    }
+
+    public boolean hasStock(Product product, int qtt) {
+        boolean stockVerify;
+        int prodIndex;
+        prodIndex = productsList.indexOf(product);
+
+        if (productsList.get(prodIndex).getStock() <= qtt) stockVerify = false;
+        else {
+            productsList.get(prodIndex).stockUpdate(qtt);
+            stockVerify = true;
+        }
+
+        return stockVerify;
+    }
+}
