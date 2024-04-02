e87cd84603f4fec53abe82f3c028e903f65c5b9f
Aurora <AurorinhaBoreal@users.noreply.github.com>
2024-04-02 13:45:14 -0300

    :construction: Construction of Order

---
+++ app/src/main/java/exercises/superMarket/Order.java
@@ -0,0 +1,40 @@
+package exercises.superMarket;
+
+import java.util.ArrayList;
+
+public class Order {
+    private ArrayList<Item> itemList = new ArrayList<>();
+    private double totalOrderValue = 0;
+
+    public void calcTotalValue() {
+        double calcOrderValue[] = {0};
+        itemList.forEach((item) -> {
+            calcOrderValue[0] += itemList.get(itemList.indexOf(item)).totalValue();
+        });
+        this.totalOrderValue = calcOrderValue[0];
+    }
+
+    public boolean addItem(Product product, int qtt, double itemValue) {
+        boolean AddVerify;
+        if (itemList.add(new Item(product, qtt, itemValue))) AddVerify = true;
+        else AddVerify = false;
+        return AddVerify;
+    }
+
+    public String toString() {
+        itemList.forEach((item)-> {
+            System.out.println("| "+itemList.get(itemList.indexOf(item)).getItem()+" |");
+        });
+        System.out.println("| TOTAL: "+this.totalOrderValue+" |");
+        return "a";
+    };
+
+    public String getTotalOrderValue() {
+        return "| "+this.totalOrderValue+" |";
+    }
+
+    public void clearCart() {
+        itemList.clear();
+        this.totalOrderValue = 0;
+    }
+}
